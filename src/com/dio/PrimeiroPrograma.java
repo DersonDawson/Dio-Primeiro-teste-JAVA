e4197789841cbe6c621e1df9267797718693cfba
Anderson <andersonsmurf@gmail.com>
2021-11-28 10:31:00 -0300

    First commit

---
+++ src/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,20 @@
+package com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		Gato gato = new Gato();
+		Livro livro = new Livro();
+		
+	    System.out.println(gato);
+	    System.out.println(livro);}}
+	    
+		// TODO Auto-generated method stub
+		/*int a = 2;
+		int b = 3;	
+		System.out.println("Hello world! " + (a+b));*/
+	
+		
+
