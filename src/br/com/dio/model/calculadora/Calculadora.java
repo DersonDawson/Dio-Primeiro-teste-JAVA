1367c456c703083116362f1d11ada1fc4dac8df7
Anderson <andersonsmurf@gmail.com>
2021-11-29 06:24:19 -0300

    Criação Calculadora

---
+++ src/br/com/dio/model/calculadora/Calculadora.java
@@ -0,0 +1,42 @@
+package br.com.dio.model.calculadora;
+
+import java.util.Scanner;
+
+public class Calculadora {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		/*calculator*/
+		double a, b;
+		System.out.println("Digite o primeiro valor: '");
+		a= scan.nextInt();
+		System.out.println("Digite o segundo valor: ");
+		b= scan.nextInt();
+		
+		double soma = soma(a,b);
+		double subtracao = subtracao(a,b);
+		double multiplicacao = multiplicacao(a,b);
+		double divisao = divisao(a,b);
+		System.out.println("Soma: " + soma);
+		System.out.println("subtracao: " + subtracao);
+		System.out.println("multiplicacao: " + multiplicacao);
+		System.out.println("divisao: " + divisao);
+	}
+
+	public static double soma(double a, double b) {
+			return a+b;
+	}
+
+	public static double subtracao(double a, double b) {
+				return a-b;
+	}
+	public static double divisao(double a, double b) {
+					return a/b;
+	}
+	public static double multiplicacao(double a, double b) {
+		return a * b;
+	}
+
+}
+
+
